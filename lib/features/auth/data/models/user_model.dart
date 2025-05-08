1787eb61c7a03c6c007c07f4c6d3dafdc02bd9d1
Salman Ahmad <salmanahma9990@gmail.com>
2025-05-08 17:41:54 +0500

    Add UserEntity and UserModel classes for user data management

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,32 @@
+import 'package:socialix_flutter_nodejs/features/auth/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required super.id,
+    required super.name,
+    required super.email,
+    required super.phone,
+    required super.profilePicture,
+    required super.bio,
+  });
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      phone: json['phone'] as String,
+      profilePicture: json['profilePicture'] as String,
+      bio: json['bio'] as String,
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'profilePicture': profilePicture,
+      'bio': bio,
+    };
+  }
+}
---
+++ lib/features/auth/domain/entities/user_entity.dart
@@ -0,0 +1,18 @@
+class UserEntity {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+  final String profilePicture;
+  final String bio;
+
+  UserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.profilePicture,
+    required this.bio,
+  });
+
+}

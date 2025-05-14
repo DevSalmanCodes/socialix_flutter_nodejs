d238e064c972c05059e2678cf55108576aceab2b
Salman Ahmad <salmanahma9990@gmail.com>
2025-05-14 09:57:26 +0500

    Add SignUpUser use case and integrate with AuthBloc for user registration

---
+++ lib/features/auth/domain/usecases/sign_up_user.dart
@@ -0,0 +1,13 @@
+import 'package:socialix_flutter_nodejs/features/auth/domain/entities/user_entity.dart';
+import 'package:socialix_flutter_nodejs/features/auth/domain/repositories/auth_repository.dart';
+
+class SignUpUser {
+  final AuthRepository authRepository;
+  SignUpUser({required this.authRepository});
+  Future<UserEntity> call(
+    String username,
+    String email,
+    String password,
+    String imagePath,
+  ) => authRepository.signUpUser(username, email, password, imagePath);
+}
--- lib/features/auth/presentation/blocs/auth_bloc.dart
+++ lib/features/auth/presentation/blocs/auth_bloc.dart
@@ -1,38 +1,50 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socialix_flutter_nodejs/features/auth/domain/usecases/sign_up_user.dart';
 import 'package:socialix_flutter_nodejs/features/auth/presentation/blocs/auth_event.dart';
 import 'package:socialix_flutter_nodejs/features/auth/presentation/blocs/auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitialState()) {
+  final SignUpUser signUpUser;
+  AuthBloc({required this.signUpUser}) : super(AuthInitialState()) {
     on<LoginRequestEvent>(_onLoginRequested);
     on<SignUpRequestEvent>(_onSignUpRequested);
     on<LogoutRequestEvent>(_onLogoutRequested);
   }
 
   Future<void> _onLoginRequested(
-      LoginRequestEvent event, Emitter<AuthState> emit) async {
+    LoginRequestEvent event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(AuthLoadingState());
-    try {
-    } catch (e) {
+    try {} catch (e) {
       emit(AuthErrorState(error: "Login Failed"));
     }
   }
 
   Future<void> _onSignUpRequested(
-      SignUpRequestEvent event, Emitter<AuthState> emit) async {
+    SignUpRequestEvent event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(AuthLoadingState());
     try {
-      // TODO: Add signup logic
+      final res = await signUpUser(
+        event.username,
+        event.email,
+        event.password,
+        event.imagepath,
+      );
+      emit(AuthSuccessState(data: res));
     } catch (e) {
-      emit(AuthErrorState(error: "Sign Up Failed"));
+      emit(AuthErrorState(error: e.toString()));
     }
   }
 
   Future<void> _onLogoutRequested(
-      LogoutRequestEvent event, Emitter<AuthState> emit) async {
+    LogoutRequestEvent event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(AuthLoadingState());
-    try {
-    } catch (e) {
+    try {} catch (e) {
       emit(AuthErrorState(error: "Logout Failed"));
     }
   }
